544ad80bd2f3cc7fcded8eac853c35f9762dd3ef
José Oliveira dos Santos <jose.victor@vvt.local>
2023-03-23 14:38:54 -0300

    Criar classes de cenário 2

--- assets/js/classes.js
+++ assets/js/classes.js
@@ -69,13 +69,26 @@ class Stage {
 
     start(){
         this.update();
-        // TODO: Evento do botão de atacar
+        
+        this.fighter1El.querySelector('.attackButton').addEventListener('click', () => this.doAttack(this.fighter1, this.fighter2));
+        this.fighter2El.querySelector('.attackButton').addEventListener('click', () => this.doAttack(this.fighter2, this.fighter1));
     }
 
     update(){
         // fighter 1
-        this.fighter1El.querySelector('.name').innerHTML = this.fighter1.name;
+        this.fighter1El.querySelector('.name').innerHTML = `${this.fighter1.name} - ${this.fighter1.life} HP`;
+        let f1Pct = (this.fighter1.life / this.fighter1.maxLife) * 100;
+        this.fighter1El.querySelector('.bar').style.width = `${f1Pct}%`;
+
         // fighter 2
-        this.fighter2El.querySelector('.name').innerHTML = this.fighter2.name;
+        this.fighter2El.querySelector('.name').innerHTML = `${this.fighter2.name} - ${this.fighter2.life} HP`;
+        let f2Pct = (this.fighter2.life / this.fighter2.maxLife) * 100;
+        this.fighter2El.querySelector('.bar').style.width = `${f2Pct}%`;
+    }
+
+    doAttack(attacking, attacked){
+        console.log(`${attacking.name} está atacando ${attacked.name}`)
+
+        this.update();
     }
 }
